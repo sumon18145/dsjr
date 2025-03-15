3948c363ccac972c8fe932b9bbac7b5d307efc92
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-03-16 03:49:26 +0600

    Air data

---
+++ R/Air_data.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(hms)
+library(readxl)
+library(lubridate)
+
+city_names <- excel_sheets("data/Air Quality Data.xlsx")
+
+air_data <- lapply(city_names, function(city) {
+  read_excel("data/Air Quality Data.xlsx", 
+             sheet = city) %>%
+    mutate(CityName = city) 
+}) %>% bind_rows()
+
+glimpse(air_data)
+
+air_data <- air_data %>%
+  mutate(
+    Date = day(ymd(Date)),
+    Time = parse_hm(Time)
+         ) %>%
+  select(Date, Time, CityName, PM2.5, Temperature)
+head(air_data)
+
